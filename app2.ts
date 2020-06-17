2c0fa70e7951b466628dac6092e41d3211709c3d
huangjiaxi <huangjiaxi@oak-inc.com>
2020-06-17 10:40:17 +0800

    修改app2.ts

--- app2.ts
+++ app2.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+// 源码里说应该用 import * as express from "express";
+// 但是使用后报错，所以就改了，不用太纠结到底用哪个，哪个能用就用哪个
 const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
